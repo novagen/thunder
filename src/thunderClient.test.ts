e03126f380bbeda63186e127cee193e3ea2036fc
Christofer Engel <christofer.engel@gmail.com>
2022-08-20 19:34:49 +0200

    Added simple test.

--- src/thunderClient.test.ts
+++ src/thunderClient.test.ts
@@ -4,6 +4,10 @@ import { ThunderClient } from '../src';
 const client = new ThunderClient();
 
 describe('Config', () => {
+    it(`Should be able to call start`, () => {
+        expect(client.start()).to.equal(undefined);
+    });
+
     it(`Should be able to call stop`, () => {
         expect(client.stop()).to.equal(undefined);
     });
